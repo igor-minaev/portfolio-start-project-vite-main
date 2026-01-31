4fbc2f8e73b764fa50d4b447d522eeb06798c1df
Igor Minayeu <igor.minayeu@gmail.com>
2026-01-31 20:30:10 +0300

    add Icon component

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,10 +1,10 @@
+import {Icon} from "../icon/Icon.tsx";
 
 
 export const Logo = () => {
     return (
         <a href="">
-            <img src=""
-                 alt=""/>
+            <Icon iconId="code"/>
         </a>
     );
 };
