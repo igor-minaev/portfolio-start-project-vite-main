345a8e83e198db369db447bfbb55aa9b10fe13c7
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-01 14:30:14 +0300

    add FlexWrapper component

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,16 +1,38 @@
 import photo from "../../../assets/images/photo.jpg"
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Hi There</span>
-                <h2>I am Ihar Minayeu</h2>
-                <h1>A Web Developer.</h1>
-            </div>
-            <img src={photo}
-                 alt=""/>
-        </div>
+        <StyledMain>
+            <FlexWrapper align="center" justify="space-around">
+                <div>
+                    <span>Hi There</span>
+                    <Name>I am Ihar Minayeu</Name>
+                    <MainTitle>A Web Developer.</MainTitle>
+                </div>
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
     );
 };
 
+// section or div
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: rgba(255, 253, 129, 0.82);
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+
+`
+
+const Name = styled.h2`
+
+`
