5167ab876e6a3f64481bde2ce9bb4509be1b1111
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-02 21:38:55 +0300

    refactor Slogan component

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,14 +1,18 @@
 import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {Button} from "../../../components/Button.tsx";
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-
+            <SectionTitle>I Am Available For Freelance</SectionTitle>
+            <Button>Hire me</Button>
         </StyledSlogan>
     );
 };
 
 const StyledSlogan = styled.section`
-
+    min-height: 50vh;
+    background-color: rgb(240, 156, 255);
 `
 
