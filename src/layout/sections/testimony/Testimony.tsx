c77afb7ef30e9e66e3327af8442b9ab5983f62ad
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-02 21:04:47 +0300

    add Slider component

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -1,15 +1,23 @@
 import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {Icon} from "../../../components/icon/Icon.tsx";
+import {Slider} from "../../../components/slider/Slider.tsx";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
-
+            <SectionTitle>Testimony</SectionTitle>
+            <FlexWrapper direction="column" alignItem="center">
+                <Icon iconId="quote"/>
+                <Slider/>
+            </FlexWrapper>
         </StyledTestimony>
     );
 };
 
 const StyledTestimony = styled.section`
-    background-color: #0f6aac;
-    min-height: 100vh;
+    background-color: rgba(181, 246, 128, 0.72);
+    min-height: 70vh;
 `
 
