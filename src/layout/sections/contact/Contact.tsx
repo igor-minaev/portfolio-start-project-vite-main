9791608d16692b897a33053bc23fb264a67ab4e9
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-02 21:17:02 +0300

    add Contact component

---
+++ src/layout/sections/contact/Contact.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Contact = () => {
+    return (
+        <StyledContact>
+
+        </StyledContact>
+    );
+};
+
+const StyledContact = styled.div`
+`
+
