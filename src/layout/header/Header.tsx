d76c6136e159f84ddc9ca93a65d29cfce8e26975
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-02 21:30:02 +0300

    add Button component

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {Menu} from "../../components/menu/Menu.tsx";
 
-const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
+const items = ["Home", "Skills", "Works", "Testimony", "Contacts"]
 
 export const Header = () => {
     return (
--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,13 +1,36 @@
 import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {Button} from "../../../components/Button.tsx";
 
 export const Contact = () => {
     return (
         <StyledContact>
-
+            <SectionTitle>Contact</SectionTitle>
+            <StyledForm>
+                <Field placeholder="name"></Field>
+                <Field placeholder="subject"></Field>
+                <Field placeholder="message" as="textarea"></Field>
+                <Button type="submit">Send message</Button>
+            </StyledForm>
         </StyledContact>
     );
 };
 
 const StyledContact = styled.div`
+    background-color: #63d6e8;
+    min-height: 50vh;
+`
+
+const StyledForm = styled.form`
+    max-width: 500px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin: 0 auto;
+`
+
+const Field = styled.input`
+
 `
 
