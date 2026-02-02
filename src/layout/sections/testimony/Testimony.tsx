bc8d73bdb7663c4b487c2a1f5d6b3395303ee700
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-02 19:17:30 +0300

    add Testimony component

---
+++ src/layout/sections/testimony/Testimony.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Testimony = () => {
+    return (
+        <StyledTestimony>
+
+        </StyledTestimony>
+    );
+};
+
+const StyledTestimony = styled.section`
+    background-color: #0f6aac;
+    min-height: 100vh;
+`
+
