4a7b0961f01acc02af71de6504834a5622bf07ea
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-02 21:11:00 +0300

    refactor Slider component

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,14 +1,22 @@
 import styled from "styled-components";
+import {FlexWrapper} from "../FlexWrapper.tsx";
 
 export const Slider = () => {
     return (
         <StyledSlider>
-            <Slide>
-                <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit.</Text>
-                <Name>@ivan ivanow</Name>
-            </Slide>
+            <FlexWrapper>
+                <Slide>
+                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.</Text>
+                    <Name>@ivan ivanow</Name>
+                </Slide>
+            </FlexWrapper>
+            <Pagination>
+                <span> </span>
+                <span> </span>
+                <span> </span>
+            </Pagination>
         </StyledSlider>
     );
 };
@@ -16,6 +24,9 @@ export const Slider = () => {
 const StyledSlider = styled.div`
     border: 1px solid red;
     max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
 const Slide = styled.div`
@@ -26,4 +37,14 @@ const Text = styled.p`
 `
 const Name = styled.span`
 
+`
+
+const Pagination = styled.div`
+    span {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        margin: 5px;
+        background-color: #ef319b;
+    }
 `
