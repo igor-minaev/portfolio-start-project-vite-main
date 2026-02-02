6be9d0e0a2bf4ee48decf3694fb756a277b6e1d7
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-02 17:53:44 +0300

    refactor Menu component

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,28 +1,20 @@
 import styled from "styled-components";
 
 
-export const Menu = () => {
+export const Menu = (props: { menuItems: Array<string> }) => {
     return (
         <StyledMenu>
             <ul>
-                <li>
-                    <a href="">Home</a>
-                </li>
-                <li>
-                    <a href="">Skills</a>
-                </li>
-                <li>
-                    <a href="">Works</a>
-                </li>
-                <li>
-                    <a href="">Testimony</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
+                {props.menuItems.map((item, index) => (
+                        <li key={index}>
+                            <a href="">{item}</a>
+                        </li>
+                    )
+                )}
             </ul>
         </StyledMenu>
-    );
+    )
+        ;
 };
 
 const StyledMenu = styled.nav`
--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,10 +1,25 @@
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {Menu} from "../../../components/menu/Menu.tsx";
 
+
+const worksItems = ["All", "landing page", "React", "spa"]
 
 export const Works = () => {
     return (
-        <div>
+        <StyledWorks>
+            <SectionTitle>My Works</SectionTitle>
+            <Menu menuItems={worksItems}/>
+            <FlexWrapper>
 
-        </div>
+            </FlexWrapper>
+
+        </StyledWorks>
     );
 };
 
+const StyledWorks = styled.section`
+    background-color: #95e0f1;
+    min-height: 100vh;
+`
