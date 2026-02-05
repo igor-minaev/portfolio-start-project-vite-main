60cbd5946b536e5e12f0be750f40fc8c2d3e59ac
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-05 21:02:04 +0300

    refactor Header component

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,22 +1,26 @@
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {Menu} from "../../components/menu/Menu.tsx";
+import {Container} from "../../components/Container.tsx";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contacts"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexWrapper justify="space-between" alignItem="center">
+                <Logo/>
+                <Menu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
     background-color: greenyellow;
-    display: flex;
-    justify-content: space-between;
 `
 
 
