9dfec78ae2d0bc74f9ddac3a8ad300604326981b
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-05 19:52:25 +0300

    refactor Footer component

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,25 +1,44 @@
 import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon.tsx";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 
 export const Footer = () => {
     return (
         <StyledFooter>
-            <Name>Ihar</Name>
-            <SocialList>
-                <SocialItem>
-                    <SocialLink>
-                        <Icon iconId="instagram"/>
-                    </SocialLink>
-                </SocialItem>
-            </SocialList>
-            <Copyright>© 2025 Ihar Minayeu, All Rights Reserved.</Copyright>
+           <FlexWrapper direction="column" alignItem="center">
+               <Name>Ihar</Name>
+               <SocialList>
+                   <SocialItem>
+                       <SocialLink>
+                           <Icon height="21px" width="21px" viewBox="0 0 21px 21px" iconId="instagram"/>
+                       </SocialLink>
+                   </SocialItem>
+                   <SocialItem>
+                       <SocialLink>
+                           <Icon height="21px" width="21px" viewBox="0 0 21px 21px" iconId="telegram"/>
+                       </SocialLink>
+                   </SocialItem>
+                   <SocialItem>
+                       <SocialLink>
+                           <Icon height="21px" width="21px" viewBox="0 0 21px 21px" iconId="vk"/>
+                       </SocialLink>
+                   </SocialItem>
+                   <SocialItem>
+                       <SocialLink>
+                           <Icon height="21px" width="21px" viewBox="0 0 21px 21px" iconId="linkedin"/>
+                       </SocialLink>
+                   </SocialItem>
+               </SocialList>
+               <Copyright>© 2025 Ihar Minayeu, All Rights Reserved.</Copyright>
+           </FlexWrapper>
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer`
-
+    background-color: #f1a76d;
+    min-height: 30vh;
 `
 
 const Name = styled.span`
@@ -27,6 +46,8 @@ const Name = styled.span`
 `
 
 const SocialList = styled.ul`
+    display: flex;
+    gap: 30px;
 
 `
 const SocialItem = styled.li`
