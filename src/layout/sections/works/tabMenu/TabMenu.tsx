03d339f4c69ec7dbd10a58df86bef3f3fe12ee17
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-10 19:40:02 +0300

    add TabMenu component

---
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import {Link} from "../../../../components/Link.tsx";
+
+
+export const TabMenu = (props: { menuItems: Array<string> }) => {
+    return (
+        <StyledTabMenu>
+            <ul>
+                {props.menuItems.map((item, index) => (
+                        <ListItem key={index}>
+                            <Link href="">{item}</Link>
+                        </ListItem>
+                    )
+                )}
+            </ul>
+        </StyledTabMenu>
+    )
+        ;
+};
+
+const StyledTabMenu = styled.nav`
+    margin-bottom: 40px;
+    ul {
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+    }
+`;
+const ListItem = styled.li`
+`;
+
+
