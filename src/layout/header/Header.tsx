508eda6ded8dce1419a8351f0737da8690dc2f18
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-23 20:07:22 +0300

    refactor for Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,15 +1,16 @@
-import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {Container} from "../../components/Container.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 import {HeaderMenu} from "./headerMenu/HeaderMenu.tsx";
 import {MobileMenu} from "./mobileMenu/MobileMenu.tsx";
+import * as React from "react";
+import {S} from "./Header_Styles.ts";
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contacts"]
 
-export const Header = () => {
+export const Header: React.FC = () => {
     return (
-        <StyledHeader>
+        <S.Header>
             <Container>
                 <FlexWrapper justify="space-between" alignItem="center">
                     <Logo/>
@@ -17,18 +18,10 @@ export const Header = () => {
                     <MobileMenu headerMenuItems={items}/>
                 </FlexWrapper>
             </Container>
-        </StyledHeader>
+        </S.Header>
     );
 };
 
-const StyledHeader = styled.header`
-    background: rgba(31, 31, 32, 0.9);
-    padding: 20px 0;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 99999;
-`
+
 
 
