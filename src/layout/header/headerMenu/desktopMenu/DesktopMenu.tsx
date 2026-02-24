8189e810a1fd1775b0bf3e785f54ae9675fb9165
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-24 18:49:39 +0300

    refactor for Menu

---
+++ src/layout/header/headerMenu/desktopMenu/DesktopMenu.tsx
@@ -0,0 +1,15 @@
+import {Menu} from "../menu/Menu.tsx";
+import React from "react";
+import {S} from './../HeaderMenu_Styles.ts'
+
+
+export const DesktopMenu: React.FC<{ menuItems: Array<string> }> = (props: { menuItems: Array<string> }) => {
+    return (
+        <S.DesktopMenu>
+            <Menu menuItems={props.menuItems}/>
+        </S.DesktopMenu>
+    )
+        ;
+};
+
+
