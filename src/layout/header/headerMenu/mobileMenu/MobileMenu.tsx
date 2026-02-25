7447fb3755585661ae3a7841c1ada91a9a710efd
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-25 18:43:10 +0300

    add functions for mobile menu

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,15 +1,17 @@
 import {Menu} from "../menu/Menu.tsx";
-import React from "react";
+import React, {useState} from "react";
 import {S} from './../HeaderMenu_Styles.ts'
 
 
 export const MobileMenu: React.FC<{ menuItems: Array<string> }> = (props: { menuItems: Array<string> }) => {
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+    const onBurgerBtnClick = () => setMenuIsOpen(!menuIsOpen)
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={false}>
+            <S.BurgerButton onClick={onBurgerBtnClick} isOpen={menuIsOpen}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup isOpen={false}>
+            <S.MobileMenuPopup isOpen={menuIsOpen} onClick={() => setMenuIsOpen(!menuIsOpen)}>
                 <Menu menuItems={props.menuItems}/>
             </S.MobileMenuPopup>
         </S.MobileMenu>
